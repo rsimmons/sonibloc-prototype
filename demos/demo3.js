a5d787b015ac8798c5d4c91255ea93311b0d38a1
Russel Simmons <symstym@mac.com>
2014-10-23 21:21:48 -0700

    add wrapped qwerty hancock bloc

---
+++ demos/demo3.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  initialize: function(audioContext, container) {
+
+//SHOWBEGIN
+// require and create qwerty hancock bloc (with view)
+var qwerty = require('./blocs/qwerty-hancock.js');
+var qwertyProc = qwerty.createProcessor(audioContext);
+var qwertyView = qwerty.createView(qwertyProc, container);
+
+// require and create lumberjack bloc
+var lumber = require('./blocs/lumberjack.js');
+var lumberProc = lumber.createProcessor(audioContext);
+
+// connect up blocs
+qwertyProc.outputs.midi.connect(lumberProc.inputs.midi);
+lumberProc.outputs.audio.node.connect(audioContext.destination);
+//SHOWEND
+
+    return function terminate() {
+      lumberProc.outputs.audio.disconnect();
+    };
+  },
+}
