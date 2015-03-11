0a3f9e584d773856806a3165f44b3622472faf82
Russel Simmons <symstym@mac.com>
2015-03-11 12:22:15 -0700

    move dawson back to react. actually create blocs

--- blocs/dawson/index.js
+++ blocs/dawson/index.js
@@ -1,78 +1,10 @@
 'use strict';
 
 var sonibloc = require('sonibloc');
-var uid = require('uid');
-// var newId = uid(12);
-
-var availableBlocs = [
-  {name: 'beatclock', _class: require('sonibloc-beatclock')},
-  {name: 'bassline', _class: require('sonibloc-bassline')},
-  {name: 'lumberjack', _class: require('sonibloc-lumberjack')},
-  {name: 'feedbackdelay', _class: require('sonibloc-feedbackdelay')},
-  {name: 'qwerty-hancock', _class: require('sonibloc-qwerty-hancock')},
-];
+var dawson = require('./build/dawson');
 
 module.exports = sonibloc.createBloc(
   function() {
-    var _thisBloc = this;
-    // var midiOut = this.addMidiOutput('midi');
-
-    var document = this.container.ownerDocument;
-
-    var blocListElem = document.createElement('div');
-    this.container.appendChild(blocListElem);
-
-    var selectElem = document.createElement('select');
-    // add initial empty option
-    selectElem.appendChild(document.createElement('option'));
-
-    for (var i = 0; i < availableBlocs.length; i++) {
-      var optionElem = document.createElement('option');
-      optionElem.textContent = availableBlocs[i].name;
-      selectElem.appendChild(optionElem);
-    }
-
-    var labelElem = document.createElement('label');
-    labelElem.textContent = 'Add ';
-    labelElem.appendChild(selectElem);
-
-    var formElem = document.createElement('form');
-    formElem.appendChild(labelElem);
-    this.container.appendChild(formElem);
-
-    selectElem.addEventListener('change', function() {
-      if (selectElem.selectedIndex > 0) {
-        var selBloc = availableBlocs[selectElem.selectedIndex-1];
-        console.log('adding bloc:', selBloc.name);
-
-        var blocOuterElem = document.createElement('div');
-        blocOuterElem.className = 'bloc-outer';
-
-        var blocInfoElem = document.createElement('div');
-        blocInfoElem.className = 'bloc-info';
-        blocInfoElem.textContent = selBloc.name;
-        blocOuterElem.appendChild(blocInfoElem);
-
-        var blocRemoveElem = document.createElement('button');
-        blocRemoveElem.className = 'bloc-remove';
-        blocRemoveElem.textContent = 'X';
-        blocOuterElem.appendChild(blocRemoveElem);
-
-        var blocContainerElem = document.createElement('div');
-        blocContainerElem.className += 'bloc-container';
-        blocOuterElem.appendChild(blocContainerElem);
-
-        blocListElem.appendChild(blocOuterElem);
-
-        // strangely, one bloc needed this to happen last, because it searched for an element by id page-wide. should probably fix that
-        var newBloc = selBloc._class.create(_thisBloc.audioContext, blocContainerElem);
-
-        if (!blocContainerElem.hasChildNodes()) {
-          blocContainerElem.className += ' bloc-container-empty';
-          blocContainerElem.textContent = '(no interface)';
-        }
-      }
-      selectElem.selectedIndex = 0; // reset to blank option
-    }, false);
+    dawson.renderApp(this.audioContext, this.container);
   }
 );
--- blocs/dawson/src/dawson.js
+++ blocs/dawson/src/dawson.js
@@ -3,9 +3,26 @@
 var React = require('react');
 var uid = require('uid');
 
-var BlocContainer = React.createClass({
+var availableBlocs = [
+  {name: 'beatclock', _class: require('sonibloc-beatclock')},
+  {name: 'bassline', _class: require('sonibloc-bassline')},
+  {name: 'lumberjack', _class: require('sonibloc-lumberjack')},
+  {name: 'feedbackdelay', _class: require('sonibloc-feedbackdelay')},
+  {name: 'qwerty-hancock', _class: require('sonibloc-qwerty-hancock')},
+];
+
+var BlocListItem = React.createClass({
   componentDidMount: function() {
-    this.refs.foo.getDOMNode().textContent = Math.random().toString();
+    var containerElem = this.refs.blocContainer.getDOMNode();
+
+    // instantiate the actual bloc
+    var newBloc = this.props.blocClass.create(this.props.audioContext, containerElem);
+
+    // if bloc didn't create a view/UI, then put placeholder stuff
+    if (!containerElem.hasChildNodes()) {
+      containerElem.className += ' bloc-container-empty';
+      containerElem.textContent = '(no interface)';
+    }
   },
 
   submitRemove: function(e) {
@@ -15,12 +32,12 @@ var BlocContainer = React.createClass({
 
   render: function() {
     return (
-      <div>
-        <span>{this.props.name}</span> &nbsp;
-        <span ref="foo">foo</span> &nbsp;
+      <div className="bloc-outer">
+        <div className="bloc-info">{this.props.name}</div>
         <form className="inlined" onSubmit={this.submitRemove}>
-          <button>Remove</button>
+          <button className="bloc-remove">X</button>
         </form>
+        <div ref="blocContainer" className="bloc-container" />
       </div>
     );
   },
@@ -31,11 +48,20 @@ var DawsonApp = React.createClass({
     return {blocs: []};
   },
 
-  submitAddBloc: function(e) {
-    e.preventDefault();
-    var newId = uid(12);
-    var newBlocs = this.state.blocs.concat({id: newId, name: 'name-' + newId});
-    this.setState({blocs: newBlocs});
+  onAddBlocSelectChange: function(e) {
+    var selectElem = this.refs.addBlocSelect.getDOMNode();
+
+    if (selectElem.selectedIndex > 0) {
+      var selBloc = availableBlocs[selectElem.selectedIndex-1];
+      var newId = uid(12);
+      var newBlocs = this.state.blocs.concat({id: newId, blocClass: selBloc._class, name: selBloc.name + '-' + newId.substring(0, 5)});
+
+      console.log('adding bloc', selBloc.name, 'id', newId);
+
+      this.setState({blocs: newBlocs});
+    }
+
+    selectElem.selectedIndex = 0; // reset to blank option
   },
 
   removeBlocId: function(blocId) {
@@ -61,18 +87,24 @@ var DawsonApp = React.createClass({
     var thisApp = this;
     return (
       <div>
-        <h1>Dawson</h1>
-        <div ref="bloc-list">
+        <div>
           {this.state.blocs.map(function(i) {
-            return <BlocContainer
+            return <BlocListItem
               key={i.id}
               name={i.name}
+              blocClass={i.blocClass}
+              audioContext={thisApp.props.audioContext}
               removeFunc={function() { thisApp.removeBlocId(i.id); }}
             />;
           })}
         </div>
-        <form onSubmit={this.submitAddBloc}>
-          <button>Add Bloc</button>
+        <form>
+          <label>Add <select ref="addBlocSelect" onChange={this.onAddBlocSelectChange}>
+            <option />
+            {availableBlocs.map(function(i) {
+              return <option key={i.name}>{i.name}</option>;
+            })}
+          </select></label>
         </form>
       </div>
     );
@@ -80,7 +112,7 @@ var DawsonApp = React.createClass({
 });
 
 module.exports = {
-  renderApp: function(container) {
-    React.render(<DawsonApp />, container);
+  renderApp: function(audioContext, container) {
+    React.render(<DawsonApp audioContext={audioContext} />, container);
   },
 }
